4c79f3633310ec54a9fabcd8c8ea8b6113242da6
Manoel Rodrigo Cândido <rodrigocan@gmail.com>
2022-11-21 16:04:55 -0300

    initial commit with superplate

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import { Refine } from "@pankod/refine-core";
+import {
+  AuthPage,
+  notificationProvider,
+  Layout,
+  ReadyPage,
+  ErrorComponent,
+} from "@pankod/refine-antd";
+
+import { DataProvider } from "@pankod/refine-strapi-v4";
+import "@pankod/refine-antd/dist/styles.min.css";
+import routerProvider from "@pankod/refine-react-router-v6";
+
+import { authProvider, axiosInstance } from "./authProvider";
+import { API_URL } from "./constants";
+
+function App() {
+  return (
+    <Refine
+      authProvider={authProvider}
+      dataProvider={DataProvider(API_URL + `/api`, axiosInstance)}
+      LoginPage={AuthPage}
+      notificationProvider={notificationProvider}
+      Layout={Layout}
+      ReadyPage={ReadyPage}
+      catchAll={<ErrorComponent />}
+      routerProvider={routerProvider}
+    />
+  );
+}
+
+export default App;
